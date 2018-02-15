5035abcdb3c48908c37cd93d3ea18661edd10f69
venkatkilaparthi <venkat.kilaparthi.7@gmail.com>
2018-02-15 08:20:50 +0530

    hi

---
+++ app/src/main/java/com/example/admin/journeywheels/Bike_Home.java
@@ -0,0 +1,66 @@
+package com.example.admin.journeywheels;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Bike_Home extends AppCompatActivity {
+
+    Button b1,b2,b3,b4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike__home);
+
+        b1=findViewById(R.id.bt1);
+        b2=findViewById(R.id.bt2);
+        b3=findViewById(R.id.bt3);
+        b4=findViewById(R.id.bt4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(Bike_Home.this,Bike_1.class);
+                startActivity(i);
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(Bike_Home.this,Bike_1.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(Bike_Home.this,Bike_1.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(Bike_Home.this,Bike_1.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/admin/journeywheels/MainActivity.java
+++ app/src/main/java/com/example/admin/journeywheels/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.admin.journeywheels;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    TextView t,t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this
+
+        t=findViewById(R.id.txt);
+        t1=findViewById(R.id.txt1);
+        image = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.zoom);
+        Animation myAnimation = AnimationUtils.loadAnimation(this,R.anim.rota);
+        Animation myAnimation1 = AnimationUtils.loadAnimation(this,R.anim.rota);
+        t.startAnimation(myAnimation);
+        t1.startAnimation(myAnimation1);
+        image.startAnimation(myanim);
+
+        //Toast.makeText(this, "Successfully done", Toast.LENGTH_SHORT).show();
+
+        final Intent i=new Intent(this,Bike_Home.class);
+        Thread timer = new Thread(){
+            public void run () {
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+
+
+            }
+
+        };
+        timer.start();
+
     }
 }
+
+
