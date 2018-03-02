b54ec9cb9022aa15c97d136e66dd96217606c028
venkat <venkatkilaparthi7@gmail.com>
2018-03-02 11:51:18 +0530

    wheels

--- app/src/main/java/com/example/admin/journeywheels/FZ1.java
+++ app/src/main/java/com/example/admin/journeywheels/FZ1.java
@@ -1,13 +1,31 @@
 package com.example.admin.journeywheels;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FZ1 extends AppCompatActivity {
+
+    Button b8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fz1);
+
+        b8=findViewById(R.id.bt8);
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1=new Intent(FZ1.this,FZ2.class);
+                startActivity(intent1);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/admin/journeywheels/FZ2.java
+++ app/src/main/java/com/example/admin/journeywheels/FZ2.java
@@ -1,13 +1,33 @@
 package com.example.admin.journeywheels;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FZ2 extends AppCompatActivity {
+
+    Button b9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fz2);
+
+        b9=findViewById(R.id.bt9);
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1=new Intent(FZ2.this,Bike_Home.class);
+                Toast.makeText(FZ2.this, "Successfully Booked", Toast.LENGTH_SHORT).show();
+                startActivity(intent1);
+
+            }
+        });
+        
     }
 }
